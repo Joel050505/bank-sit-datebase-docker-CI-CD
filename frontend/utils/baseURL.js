017e95206e3d326c3eff310dbc687466ad5c16fc
Your Name <you@example.com>
2025-04-07 10:21:33 +0200

    first commit

---
+++ frontend/utils/baseURL.js
@@ -0,0 +1,4 @@
+// export const BASE_URL = "localhost";
+export const BASE_URL = "ec2-51-20-109-204.eu-north-1.compute.amazonaws.com";
+
+// scp -i ~/Desktop/AWS.pem -r ~/Desktop/bank-site-with-database-main ubuntu@ec2-51-20-109-204.eu-north-1.compute.amazonaws.com:/home/ubuntu
